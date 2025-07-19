da3af615f4429088728e3242fe352f1e0a60c265
hra2zithub <hr.a2zithub@gmail.com>
2025-07-19 11:14:12 +0530

    Update dbconnection.js
    database

--- dbconnection.js
+++ dbconnection.js
@@ -1,9 +1,9 @@
 var mysql=require('mysql');
 var con=mysql.createConnection({
-    user:'root',
-    database:'hotel_project_db',
-    password:'',
-    host:'localhost',
+    user:'ujpgizs910fqdttl',
+    database:'bckqmpgxvznbln6tps13',
+    password:'e5ZGvhL5TrrTjEvZ1Lzs',
+    host:'bckqmpgxvznbln6tps13-mysql.services.clever-cloud.com',
 });
 var util=require('util');
 var execute=util.promisify(con.query).bind(con);
